810591bf4715bdcc13c76e9c9fe26f78ec009523
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-05-26 17:17:41 -0700

    Some visual looks.

--- src/shadow.js
+++ src/shadow.js
@@ -27,16 +27,33 @@ bot.on("warn", (e) => console.warn(e));
 
 
 bot.on('ready', () => { bot.events.get('ready').execute(bot); });
+
+  //Message Stuff.
+
 bot.on('message', (message) => { bot.events.get('message').execute(bot, message); });
+
+
 bot.on("messageDelete", (message) => { bot.events.get('messageDeleted').execute(bot, message); });
+
+
 bot.on("messageBulkDelete", (messages) => { bot.events.get('messageBulkDelete').execute(bot, messages); });
+
+
 bot.on("messageUpdate", (oldMessage, newMessage) => { bot.events.get('messageUpdated').execute(bot, oldMessage, newMessage); });
 
+  //Guild Stuff
+
 bot.on('guildCreate', (guild) => { bot.events.get('newGuild').execute(bot, guild); });
+
+
 bot.on('guildDelete', (guild) => { bot.events.get('oldGuild').execute(bot, guild); });
 
+
 bot.on("guildMemberAdd", (member) => { bot.events.get('newMember').execute(bot, member); });
+
+
 bot.on("guildMemberRemove", (member) => { bot.events.get("oldMember").execute(bot, member); });
+
 
 bot.on("guildUpdate", (oldGuild, newGuild) => {  });
 
