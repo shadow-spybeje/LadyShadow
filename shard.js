ef4c973cd027a79233fc860e16baf73827eee143
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-05-28 03:16:48 -0700

    shard.respawn = true

--- shard.js
+++ shard.js
@@ -9,7 +9,7 @@ const token = require('../.././tokens.json').LadyShadow;
 const { ShardingManager } = require('discord.js');
 
 const shard = new ShardingManager('./src/shadow.js', { token: token,
-  respawn: false
+  respawn: true
 });
 
 shard.spawn();
