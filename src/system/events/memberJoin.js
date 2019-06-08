26de124dd3dab8c4b1aca322fa34da58b01be0c8
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-06-08 16:12:16 -0700

    When a "Blacklisted" user joins Support. (Update soon to any server. 'welcome channel)

--- src/system/events/memberJoin.js
+++ src/system/events/memberJoin.js
@@ -8,6 +8,24 @@ module.exports = {
     execute(bot, member){
         settings = bot.settings.g.get(member.guild.id);
 
+        if(member.guild.id == '416906584900239370'){
+          bl = false;
+          bl = bot.blacklisted.some(id => { if(member.user.id == id) return true; });
+          if(bl){
+            e = new discord.RichEmbed();
+            e.setTitle("⚠");
+            e.setColor("ff0000");
+            e.setDescription(`⚠ Member Warning ⚠\n\nThis user <@${member.id}> has been blacklisted among ${bot.blacklisted.length} others.\n\n**@Staffers**, Consolt a Server **@${member.guild.roles.get('417909130171383809').name}** before taking any action.`);
+            e.setFooter("*Well that's funny... Weren't they banned??*");
+
+            member.guild.channels.get("416906777049825292").send(e);
+
+            member.addRole("586839954173198336", "New-Blacklisted-User -- Give \"BlackListed\" Role.")
+            .catch(err => member.guild.channels.get("416906777049825292").send(err));
+          };
+        };
+
+
         if(!settings.welcome) return;
         //if(!settings.wMsg) {
           wMsg = `<@${member.user.id}> has joined the server!!\nNew Member Count : \`(${member.guild.members.size})\``;
@@ -15,6 +33,7 @@ module.exports = {
           //wMsg = settings.wMsg;
         //};
 
+        e = new discord.RichEmbed();
         e.setAuthor(member.user.tag + ` (${member.user.id})`, member.user.avatarURL);
         e.setThumbnail(member.user.avatarURL);
         e.setColor("GREEN");
