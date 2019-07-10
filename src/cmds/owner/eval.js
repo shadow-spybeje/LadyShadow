8bc3027327f5cd025e9194842a001d0d5536422c
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2019-07-10 00:48:50 -0700

    code outpus in `js` instead of `css`

--- src/cmds/owner/eval.js
+++ src/cmds/owner/eval.js
@@ -47,7 +47,7 @@ module.exports = {
       const code = args.join(" ");
       let evaled = eval(code);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-      message.channel.send(clean(evaled), {code:"xl", split:true});
+      message.channel.send(clean(evaled), {code:"js", split:true});
     }catch(err){
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
