3a7408c2e9ad21e57af12679726957231f49abfb
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2019-08-03 02:34:03 -0700

    SRPG - Help (Basic)

---
+++ src/cmds/srpg/cmds/help.js
@@ -0,0 +1,13 @@
+module.exports = {
+    coded : "2019-07-09",
+
+    name : "help",
+    description : "S.RPG Help.",
+    usage : "[cmd]",
+
+    help : "basic",
+
+    execute(message){
+        message.channel.send("This cmd is still being created.\nPlease visit the support server for more info!!\n\n\nBasic Help\n\n• splash\n• start\n• suggest");
+    },
+};
