734928e3f434ff7e85237b261aa216d5b2689b65
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2019-08-03 02:31:51 -0700

    +SRPG Item "Criminal"
    id : 6
    Cost : 10
    Type : item
    Name : Criminal
    Desc : "The Finest Ale this isde of the Galaxy... Wait... Galaxy?!?!?!"

--- src/cmds/srpg/system/srpgItems.js
+++ src/cmds/srpg/system/srpgItems.js
@@ -10,5 +10,6 @@ exports.items = {
     "2" : { "id" : 2, "value" : 3, "type" : "item", "name" : "Cookie", "desc" : "Who doesn't like Cookies??"},
     "3" : { "id" : 3, "value" : 5, "type" : "armor", "name" : "Shirt",  "desc" : "", "stats" : { "def" : 1 } },
     "4" : { "id" : 4, "value" : 7, "type" : "weapon", "name" : "Short Dagger", "desc" : "Like a dagger. But shorter!!", "stats" : { "atk" : 1 } },
-    "5" : { "id" : 5, "value" : 0, "type" : "toy", "name" : "BoogyBear", "desc" : "........"}
+    "5" : { "id" : 5, "value" : 0, "type" : "toy", "name" : "BoogyBear", "desc" : "........"},
+    "6" : { id : 6, value : 10, type : "item", name : "Criminale", desc : "The Finest Ale this side of the Galaxy... Wait... Galaxy?!?!?!"},
 };
