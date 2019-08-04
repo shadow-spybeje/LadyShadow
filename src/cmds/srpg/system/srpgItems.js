4bcf85eb66bb8c7b5f080cb77c7b8330f2a85b17
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2019-08-03 21:17:04 -0700

    [seeDesc]
    • srpg.items file now using an array fromat rather than a json format,
    • • Help from Luke#6723 for grabbing item info.
    
    • added 'hide' to hide some items from the shop.
    
    • added 'color' to give the 'shop.view' cmd some added flavor.
    
    • other minor edits.

--- src/cmds/srpg/system/srpgItems.js
+++ src/cmds/srpg/system/srpgItems.js
@@ -5,11 +5,11 @@
  * stats = { hp : #, def : #, atk : # } // 1 or multiple.
 */
 
-exports.items = {
-    "1" : { "id" : 1, "value" : 1, "type" : "item", "name" : "Gold", "desc" : "It's Shiny!!"},
-    "2" : { "id" : 2, "value" : 3, "type" : "item", "name" : "Cookie", "desc" : "Who doesn't like Cookies??"},
-    "3" : { "id" : 3, "value" : 5, "type" : "armor", "name" : "Shirt",  "desc" : "", "stats" : { "def" : 1 } },
-    "4" : { "id" : 4, "value" : 7, "type" : "weapon", "name" : "Short Dagger", "desc" : "Like a dagger. But shorter!!", "stats" : { "atk" : 1 } },
-    "5" : { "id" : 5, "value" : 0, "type" : "toy", "name" : "BoogyBear", "desc" : "........"},
-    "6" : { id : 6, value : 10, type : "item", name : "Criminale", desc : "The Finest Ale this side of the Galaxy... Wait... Galaxy?!?!?!"},
-};
+exports.items = [
+    { hide : true, id : 1, value : 1, type : "Item", name : "Gold", color : "ffff00", desc : "It's Shiny!!"},
+    { id : 2, value : 3, type : "Item", name : "Cookie", color : "", desc : "Who doesn't like Cookies??"},
+    { id : 3, value : 5, type : "Armor", name : "Shirt",  color : "ffffff", desc : "", stats : { def : 1 } },
+    { id : 4, value : 7, type : "Weapon", name : "Short Dagger", color : "a9a9a9", desc : "Like a dagger.\n- But shorter!!", stats : { atk : 1 } },
+    { hide : true, id : 5, "value" : 0, type : "Toy", name : "BoogyBear", color : "", desc : "........"},
+    { hide : true, id : 6, value : 10, type : "Item", name : "Criminale", color : "", desc : "The Finest Ale this side of the Galaxy... Wait... Galaxy?!?!?!"},
+];
