e9f6eefb36313bcea530d4dd482c7be590a1e94c
§π∆d°≈~§¶¥#1904 <29105063+ScionSpy@users.noreply.github.com>
2019-08-17 00:57:57 -0700

    worked out a bug, gave a celebrant response.

--- src/cmds/general/celebrate.js
+++ src/cmds/general/celebrate.js
@@ -24,6 +24,18 @@ module.exports = {
         userInfo = userF(u.id, u.tag)
 
         ch = bot.channels.get("569001544926887946");
-        ch.send(userInfo ,{code:"js"});
+        ch.send(`{\n\ \ ${userInfo.id}\n\ \ ${userInfo.tag}\n}` ,{code:"js"});
+
+
+        ch.fetchMessage("612187492820582410").then(msg => {
+            let num = Number(msg.content);
+            if(!isNaN(num)){
+                num++
+
+                msg.edit(num);
+                message.channel.send(`You're person ${num} to celebrate Lillith's 4'th birthday!!\n🎉🎂🎊`);
+            };
+
+        }).catch(console.error);
     },
 };
